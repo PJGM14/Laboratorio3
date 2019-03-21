4644bbe5d3bf2f6c29fb0a886ef496b05d254641
PJGM14 <pablitolink@hotmail.com>
2019-03-21 11:12:27 -0600

    EXAMPLE MODEL

---
+++ Lab3_RicardoChian_PabloGarcia/Models/ExampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab3_RicardoChian_PabloGarcia.Models
+{
+    public class ExampleModel
+    {
+    }
+}
