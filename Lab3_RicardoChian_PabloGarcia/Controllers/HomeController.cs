6be8ad9c3430e0836a9b30e8dce3faa52aff4f9b
PJGM14 <pablitolink@hotmail.com>
2019-03-21 11:40:23 -0600

    Empty project, deleted design

--- Lab3_RicardoChian_PabloGarcia/Controllers/HomeController.cs
+++ Lab3_RicardoChian_PabloGarcia/Controllers/HomeController.cs
@@ -13,18 +13,5 @@ namespace Lab3_RicardoChian_PabloGarcia.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
