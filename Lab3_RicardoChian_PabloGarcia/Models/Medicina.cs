9f4383faaef5e86e20c9776d41b118e6fe67fcf4
Ricardo Chian <ricardochianc@gmail.com>
2019-03-27 20:59:34 -0600

    Cambios en el modelo medicina

--- Lab3_RicardoChian_PabloGarcia/Models/Medicina.cs
+++ Lab3_RicardoChian_PabloGarcia/Models/Medicina.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
 using System.Web;
+using Estructuras.NoLinearStructures.Interface;
 
 namespace Lab3_RicardoChian_PabloGarcia.Models
 {
-    public class Medicina : IComparable
+    public class Medicina : IComparable, ITextoTamañoFijo
     {
         [Display(Name = "ID")]
         public int Id { get; set; }
@@ -27,6 +29,10 @@ namespace Lab3_RicardoChian_PabloGarcia.Models
         [Display(Name = "Existecias")]
         public int Existencia { get; set; }
 
+        //ID-NOMBRE-DESCRIPCION-CASA-PRECIO-EXISTENCIAS
+        //10-  40  -     60    - 40 -  10  -      10    = 175
+        private const string FormatoConst = "0000000000-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-0000000000-0000000000";
+
         public Medicina(string id, string nombre, string descripcion, string casa, string precio, string existencia)
         {
             Id = Int32.Parse(id);
@@ -35,9 +41,8 @@ namespace Lab3_RicardoChian_PabloGarcia.Models
             Casa = casa;
 
             var aux = precio.Substring(1);
-
             Precio = Double.Parse(aux);
-            //Precio = double.Parse(precio, CultureInfo.InvariantCulture);
+
             Existencia = Int32.Parse(existencia);
         }
 
@@ -52,5 +57,45 @@ namespace Lab3_RicardoChian_PabloGarcia.Models
         {
             return Med1.CompareTo(Med2);
         };
+
+        public int FixedSizeText
+        {
+            get
+            {
+                return 175;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return ToFixedSizeString();
+        }
+
+        public string ToFixedSizeString()
+        {
+            var strgBuilder = new StringBuilder();
+
+            strgBuilder.Append(Id.ToString().PadLeft(10, '0'));
+            strgBuilder.Append('-');
+            strgBuilder.Append(Nombre.PadLeft(40, 'x'));
+            strgBuilder.Append('-');
+            strgBuilder.Append(Descripcion.PadLeft(60, 'x'));
+            strgBuilder.Append('-');
+            strgBuilder.Append(Casa.PadLeft(40, 'x'));
+            strgBuilder.Append('-');
+            strgBuilder.Append(Precio.ToString().PadLeft(10, '0'));
+            strgBuilder.Append('-');
+            strgBuilder.Append(Existencia.ToString().PadLeft(10, '0'));
+
+            return strgBuilder.ToString();
+        }
+
+        public Medicina Fabricar(string textoTamañoFijo)
+        {
+            var datos = textoTamañoFijo.Split('-');
+            var medicamento = new Medicina(datos[0],datos[1],datos[2],datos[3],datos[4],datos[5]);
+            return medicamento;
+        }
     }
 }
